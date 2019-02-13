cffde131410270a313e4afac3f627bff778e467b
glappy <glappy@naver.com>
2019-02-13 11:16:33 +0900

    first

---
+++ src/main/java/com/biz/naver/config/NaverClientSample.java
@@ -0,0 +1,8 @@
+package com.biz.naver.config;
+
+public class NaverClientSample {
+	/* 네이버 오픈 api 사용등록을 실행한 후 본인의 client id와 secret을 작성한 후 
+	 * naverclient로 파일 이름을 변경한 후 프로젝트를 실행하세요*/
+	public static final String ID="YOUR CLIENT ID";
+	public static final String KEY="YOUR CLIENT SECRET";
+}
